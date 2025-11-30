c6172631dc1e36a3109391d51e8d51b795a4057f
207253063 <gal.zuk@live.biu.ac.il>
2025-11-30 17:30:27 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  path = file_path
+  file = open(path, "r")
+  rows = file.readlines()
+  file.close()
+
+  codon_to_amino_dict = {}
+  for r in rows:
+    cells = r.strip().split('\t')
+    codon_to_amino_dict[cells[0]] = cells[2]
+  return codon_to_amino_dict
 
 
